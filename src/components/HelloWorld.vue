c4d1e3f42e333cae47002d6c7025fa3b6c1ac367
Matias <hmatias09@gmail.com>
2024-04-19 17:42:32 -0500

    tailwind added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,37 +8,15 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <header class="bg-white/15 dark:bg-primary/15 h-screen mx-auto flex relative flex-col justify-center items-center text-center px-12 gap-4 ">
+    <!-- <nav class="absolute top-0">
+      asdasd
+    </nav> -->
+    <h1 class="text-7xl sm:text-8xl md:text-9xl text-primary dark:text-dprimary"><strong>Matias Escalante</strong></h1>
+    <span class="text-3xl sm:text-4xl md:text-5xl text-primary dark:text-dprimary">Fullstack Developer</span>
+  </header>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
