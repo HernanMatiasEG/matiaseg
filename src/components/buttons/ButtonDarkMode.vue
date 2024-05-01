f637b12f2d3bcdb7dc1ce347a65539a67d2354ac
Matias <hmatias09@gmail.com>
2024-05-01 08:29:44 -0500

    Added Button

---
+++ src/components/buttons/ButtonDarkMode.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import IconMoon from '../icons/IconMoon.vue';
+import IconSun from '../icons/IconSun.vue';
+
+
+</script>
+
+<template>
+    <div class="rounded border rounded-full px-2 border-blue-400 flex">
+        <div><IconMoon /></div>
+        <div><IconSun /></div>
+    </div>
+</template>
