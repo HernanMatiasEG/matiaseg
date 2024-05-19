eceb9815432d8000a600635243b2432bb07654c0
Matias <hmatias09@gmail.com>
2024-05-19 17:56:40 -0500

    added darkmode

--- src/components/buttons/ButtonDarkMode.vue
+++ src/components/buttons/ButtonDarkMode.vue
@@ -3,12 +3,21 @@
 import IconMoon from '../icons/IconMoon.vue';
 import IconSun from '../icons/IconSun.vue';
 
+import { inject } from 'vue'
+
+const {darkmode, updateDarkmode} = inject('darkmode')
+
+// console.log(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+defineProps({
+  darkmode: {
+    type: Boolean
+  }
+})
 
 </script>
 
 <template>
-    <div class="rounded border rounded-full px-2 border-blue-400 flex">
-        <div><IconMoon /></div>
-        <div><IconSun /></div>
-    </div>
+  <i @click="updateDarkmode" v-if="darkmode" class="text-primary dark:text-dprimary"><IconMoon /></i>
+  <i @click="updateDarkmode" v-else class="text-primary dark:text-dprimary"><IconSun /></i>
 </template>
